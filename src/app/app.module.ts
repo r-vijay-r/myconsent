ca864248e3b1353add67b84c070e67846586a214
gmanojisaac <gmanoj.isaac@gmail.com>
2016-09-21 08:50:11 +0530

    Angularfire2 working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {AngularFireModule} from 'angularfire2';
+import * as firebase from 'firebase';
+import { AuthProviders,AuthMethods} from 'angularfire2';
+
+const firebaseConfig = {
+ apiKey: "AIzaSyAr0VHZmbKw_dvH5q0OB8hJElD4QZMp4s0",
+ authDomain: "myconsent-f5d0d.firebaseapp.com",
+ databaseURL: "https://myconsent-f5d0d.firebaseio.com",
+ storageBucket: "myconsent-f5d0d.appspot.com"
+}
+const AuthenticationConfig = {
+   method: AuthMethods.Popup,
+   provider: AuthProviders.Google
+ }
 
 @NgModule({
   declarations: [
@@ -12,7 +26,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig, AuthenticationConfig)
+
   ],
   providers: [],
   bootstrap: [AppComponent]
